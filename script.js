0e6ea1289c4de6c65b4fb8a22d624a34687d8e82
Akshar Gandhi <akshar.gandhi99@gmail.com>
2022-12-11 20:49:47 +0530

    added zoom functionality and favicon

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function(){
+    scpopLoad('.gallery-images');
+  });
+// const img2 = document.getElementById("floral-img-2")
+
+// function zoom() {
+//     const div = constdocument.createElement("div")
+    
+// }
