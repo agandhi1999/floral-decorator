2f4c5aad7e78b75e199ac110a074b714901c9b04
Akshar Gandhi <akshar.gandhi99@gmail.com>
2022-12-16 19:31:12 +0530

    responsive design for horizontal mobile orientation

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 window.addEventListener('load', function(){
     scpopLoad('.gallery-images');
   });
-// const img2 = document.getElementById("floral-img-2")
-
-// function zoom() {
-//     const div = constdocument.createElement("div")
-    
-// }
